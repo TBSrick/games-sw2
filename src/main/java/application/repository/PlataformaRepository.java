29d67c7db230bd55bebf8471d8e1fdb9ba5cee93
TBSrick <tbsrick1@gmail.com>
2024-10-02 17:53:44 +0000

    salve1

---
+++ src/main/java/application/repository/PlataformaRepository.java
@@ -0,0 +1,6 @@
+package application.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlataformaRepository extends JpaRepository<Plataforma, Long>{
+    
+}
